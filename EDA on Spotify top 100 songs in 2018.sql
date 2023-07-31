0d921353db54be2069d4eb2f65fb7fe67d18bff7
Adefunke Salawu <106559374+Adefunkehub@users.noreply.github.com>
2023-07-31 07:44:15 +0100

    Update EDA on Spotify top 100 songs in 2018.sql

--- EDA on Spotify top 100 songs in 2018.sql
+++ EDA on Spotify top 100 songs in 2018.sql
@@ -1,3 +1,5 @@
+---Number of Appearances of artists on the chart
+
 SELECT artists,
         COUNT(artists) numberofapperance
 
@@ -8,7 +10,7 @@ SELECT artists,
          ORDER BY
          2 DESC
 
-
+---Are there more artists in the Top 100 with 'Lil' in their name, or with 'DJ' in their name?
 SELECT 
    'Lil' AS type,
 COUNT(DISTINCT artists) AS nberofartists 
